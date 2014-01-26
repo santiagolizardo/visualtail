0577f1fed379682477ccab704e86f4ef17deabe1
Santiago Lizardo <santiago.lizardo@gmail.com>
2014-01-26 20:33:37 +0000

    License header changes

--- src/main/java/com/santiagolizardo/beobachter/gui/actions/FindAction.java
+++ src/main/java/com/santiagolizardo/beobachter/gui/actions/FindAction.java
@@ -1,19 +1,18 @@
 /**
- * Beobachter is a logs watcher for the desktop. (a.k.a. full-featured tail)
- * Copyright (C) 2013 Santiago Lizardo (http://www.santiagolizardo.com)
+ * This file is part of Beobachter, a graphical log file monitor.
  *
- * This program is free software: you can redistribute it and/or modify
+ * Beobachter is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Beobachter is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Beobachter.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.santiagolizardo.beobachter.gui.actions;
 
